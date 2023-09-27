a5dca0c4c0558f3fb9076ad4952d5b7e21a18003
object-ions <moshikoatia@gmail.com>
2023-09-27 16:18:24 -0700

    test GetAll_ReturnsListOfCities and pass.
    Co-authored-by: Ian Bravo <bravo.ian@gmail.com>

--- PlacesIveBeen.Tests/ModelTests/PlaceTests.cs
+++ PlacesIveBeen.Tests/ModelTests/PlaceTests.cs
@@ -12,6 +12,7 @@ namespace PlacesIvBeen.Tests
     {
       Place.ClearAll();
     }
+    //teardown method
 
     [TestMethod]
     public void PlaceCostructor_CreatesInstanceOfPlace_Place()
@@ -56,6 +57,25 @@ namespace PlacesIvBeen.Tests
 
       List<Place> result = Place.GetAll();
 
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsListOfCities_PlaceList()
+    {
+      //Arrange
+      string city1 = "London";
+      string city2 = "Portland";
+      Place newPlace1 = new Place(city1);
+      // newPlace1 = London;
+      Place newPlace2 = new Place(city2);
+      // newPlace2 = Portland;
+      List<Place> newList = new List<Place> { newPlace1, newPlace2 };
+
+      //Act
+      List<Place> result = Place.GetAll();
+
+      //Assert
       CollectionAssert.AreEqual(newList, result);
     }
   }
